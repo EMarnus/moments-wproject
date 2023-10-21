65e928a522448d14d185e7315499910f7082aee1
EMarnus <>
2023-10-21 16:47:50 +0000

    Add axios interceptors to CurrentUserContext

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,6 @@ axios.defaults.baseURL =
   "https://learn-django-rest-8c1f3578007f.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-date";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
