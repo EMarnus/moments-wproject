a096a11c2c5fc28bc699bd02169b207622607679
EMarnus <>
2023-10-21 10:38:54 +0000

    Add Axios api and wired up, add SignUp form and wired up to backend API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-w-b3eddd801b4b.herokuapp.com/';
+axios.defaults.baseURL = 'https://learn-django-rest-8c1f3578007f.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-date';
 axios.defaults.withCredentials = true;
