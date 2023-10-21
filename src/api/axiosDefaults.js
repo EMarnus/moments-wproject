fe9c5c52e472ac04e2f5c63b30d0860f78584017
EMarnus <>
2023-10-21 16:25:23 +0000

    Formatting on js files

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://learn-django-rest-8c1f3578007f.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-date';
+axios.defaults.baseURL =
+  "https://learn-django-rest-8c1f3578007f.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-date";
 axios.defaults.withCredentials = true;
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -18,104 +18,106 @@ import axios from "axios";
 import { useSetCurrentUser } from "../../context/CurrentUserContext";
 
 function SignInForm() {
-    const setCurrentUser = useSetCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-    const [signInData, setSignInData] = useState({
-        username: "",
-        password: "",
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const history = useHistory();
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const { username, password } = signInData;
-
-    const history = useHistory();
-
-    const [errors, setErrors] = useState({});
-
-    const handleChange = (event) => {
-        setSignInData({
-            ...signInData,
-            [event.target.name]: event.target.value,
-        });
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const {data} = await axios.post('/dj-rest-auth/login/', signInData);
-            setCurrentUser(data.user)
-            history.push('/');
-        } catch(err){
-            setErrors(err.response?.data);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
   return (
     <Row className={styles.Row}>
-        <Col className="my-auto p-0 p-md-2" md={6}>
-            <Container className={`${appStyles.Content} p-4 `}>
-            <h1 className={styles.Header}>sign in</h1>
-            
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label className="d-none">Username</Form.Label>
-                    <Form.Control 
-                        className={styles.Input}
-                        type="text" 
-                        placeholder="Username" 
-                        name="username"
-                        value={username}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.username?.map((message, idx) => (
-                    <Alert variant="warning" key={idx}>
-                        {message}
-                    </Alert>
-                ))}
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
 
-                <Form.Group controlId="password">
-                    <Form.Label className="d-none">Password</Form.Label>
-                    <Form.Control 
-                        className={styles.Input}
-                        type="password" 
-                        placeholder="Password" 
-                        name="password"
-                        value={password}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.password?.map((message, idx) => (
-                    <Alert variant="warning" key={idx}>
-                        {message}
-                    </Alert>
-                ))}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-                <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
-                    Sign in
-                </Button>
-                {errors.non_field_errors?.map((message, idx) => (
-                <Alert key={idx} variant="warning" className="mt-3">
-                    {message}
-                </Alert>
-                ))}
-            </Form>
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-            </Container>
-            <Container className={`mt-3 ${appStyles.Content}`}>
-            <Link className={styles.Link} to="/signup">
-                Don't have an account? <span>Sign up now!</span>
-            </Link>
-            </Container>
-        </Col>
-        <Col
-            md={6}
-            className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-        >
-            <Image
-            className={`${appStyles.FillerImage}`}
-            src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
-            />
-        </Col>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign in
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+        />
+      </Col>
     </Row>
   );
 }
