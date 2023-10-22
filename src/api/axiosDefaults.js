bc916c66ed24cf92601b849f8538262fa6bde307
EMarnus <>
2023-10-22 11:48:25 +0000

    Add page for creating post and wired to API

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL =
   "https://learn-django-rest-8c1f3578007f.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-date";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
